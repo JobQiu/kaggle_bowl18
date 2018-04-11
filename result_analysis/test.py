dd3da6643c04e9d745c6c2b7d5f9c0d5dc9bbe21
Congmin <congminqi@gmail.com>
2018-04-11 17:13:11 -0500

    ...

--- result_analysis/test.py
+++ result_analysis/test.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+sns.set()
 
 masks = np.load('difficultone.npy')
 masks2 = np.swapaxes(masks,1,2)
 masks2 = np.swapaxes(masks2,0,1)
-plt.imshow(masks2[0])
+for i in range(16):
+    plt.figure()
+    plt.imshow(masks2[i])
 
 
 #%%
+    
