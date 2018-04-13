2ad93c8bbd2e5a6222e94d659944ec9ceb0c281c
Congmin <congminqi@gmail.com>
2018-04-13 16:13:27 -0500

    ...

--- preprocess.py
+++ preprocess.py
@@ -105,7 +105,7 @@ import numpy as np
 import datetime
 
 problem_ids = list()
-problem_ids.append('7b38c9173ebe69b4c6ba7e703c0c27f39305d9b2910f46405993d2ea7a963b80')
+'''problem_ids.append('7b38c9173ebe69b4c6ba7e703c0c27f39305d9b2910f46405993d2ea7a963b80')
 problem_ids.append('b1eb0123fe2d8c825694b193efb7b923d95effac9558ee4eaf3116374c2c94fe')
 problem_ids.append('9bb6e39d5f4415bc7554842ee5d1280403a602f2ba56122b87f453a62d37c06e')
 problem_ids.append('1f0008060150b5b93084ae2e4dabd160ab80a95ce8071a321b80ec4e33b58aca')
@@ -113,7 +113,7 @@ problem_ids.append('58c593bcb98386e7fd42a1d34e291db93477624b164e83ab2afa3caa90d1
 problem_ids.append('adc315bd40d699fd4e4effbcce81cd7162851007f485d754ad3b0472f73a86df')
 problem_ids.append('12aeefb1b522b283819b12e4cfaf6b13c1264c0aadac3412b4edd2ace304cb40')
 problem_ids.append('0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9')
-
+'''
 
 def getNameFromTime():
     now = datetime.datetime.now()
@@ -179,7 +179,7 @@ TRAIN_PATH = 'stage1_train/'
 TEST_PATH = 'stage1_test/'
 
 train_ids = next(os.walk(TRAIN_PATH))[1]
-test_ids = next(os.walk(TEST_PATH))[1]
+#test_ids = next(os.walk(TEST_PATH))[1]
 
 print('Getting and resizing train images and masks ... ')
 
@@ -190,6 +190,10 @@ for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
     
     path = TRAIN_PATH + id_
     image__ = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    if image__.shape[0] <= 256 and image__.shape[1] <= 256:
+        continue
+    if image__.shape[0] < 256 or image__.shape[1] < 256:
+        continue
     mask_imgs = list()
     
     
